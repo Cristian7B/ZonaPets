e7f03ea23b7acabf7809a9f01209a3e0a855dd15
Cristian <criscamcr7@gmail.com>
2024-01-10 17:08:04 -0500

    Marker ZonaPets

--- PetSearch/settings.py
+++ PetSearch/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3s=e)p*@!v%#3d&6rpjh=5=xxxzu6)a*&7w(hr=iq$=jgim@&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["zonapets.onrender.com"]
+ALLOWED_HOSTS = ["zonapets.onrender.com", "127.0.0.1"]
 CSRF_TRUSTED_ORIGINS = ['https://zonapets.onrender.com','https://*.127.0.0.1']
 
 # Application definition
