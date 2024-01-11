7b9545296b3481daded95dc3f27fedb131ee9c8c
Cristian <criscamcr7@gmail.com>
2024-01-11 17:49:04 -0500

    Fix: Revision

---
+++ static/javascript/registrar.js
@@ -0,0 +1,10 @@
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu() {
+    document.getElementById("move-content").classList.toggle("move-container-all");
+    document.getElementById("move-menu").classList.toggle("show-lateral");
+
+    // Agregar código para ajustar la posición de logo-nombre durante el modo responsive
+    var logoNombre = document.querySelector(".logo-nombre");
+    logoNombre.classList.toggle("move-logo-responsive");
+}
