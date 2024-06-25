95523bbde2bda935220e19ce7fe4971d0736ea1c
Cristian <criscamcr7@gmail.com>
2024-06-25 11:36:28 -0500

    Fix

--- staticfiles_build/javascript/Info-Pages/responsivelanding.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const iconMenu = document.getElementById('icon-menu');
-    const links = document.querySelector('.links');
-
-    iconMenu.addEventListener('click', function() {
-        links.classList.toggle('show');
-    });
-});
