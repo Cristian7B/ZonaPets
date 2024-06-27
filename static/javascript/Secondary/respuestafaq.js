d987271fb90357917a92a5788915051e54c0e838
Cristian <criscamcr7@gmail.com>
2024-06-27 17:05:26 -0500

    Add: Pagina de respuesta a la pregunta del usuario. Redirección desde FaQ

---
+++ static/javascript/Secondary/respuestafaq.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon').addEventListener('click', () => {
+    document.querySelector('.mobile-links-page').classList.toggle('active');
+    document.querySelector('.menu-icon').classList.toggle('active');
+});
